9c69c7f963b50b059413db2e644367550dc4d6d1
miki <francamente@hotmail.es>
2022-09-19 23:52:13 +0200

    Ejercicio1tema1

---
+++ src/com/Miki/TiposDato.java
@@ -0,0 +1,19 @@
+package com.Miki;
+
+public class TiposDato {
+    public static void main(String[] args) {
+        byte num1 = 1;
+        short num2 = 5;
+        int num3 = 1236789699;
+        long num4 = 6566585875757875585l;
+        float num5 = 38709808.87973f;
+        double num6 = 666474343.76876;
+
+        boolean condicion = true;
+
+        char letra = 'a';
+        String texto = "hola";
+        System.out.println(num1+ " "+ num2 + " " + num3 + " " + num4 + " " + num5 + " " + num6 + " " +
+                condicion + " " + letra + " " + texto);
+    }
+}
