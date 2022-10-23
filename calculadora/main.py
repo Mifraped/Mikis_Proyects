2c33ec3af011119feb6f43ad5e3449ef6960b6c3
miki <francamente@hotmail.es>
2022-10-23 15:43:08 +0200

    ejercicio7Python

---
+++ calculadora/main.py
@@ -0,0 +1,8 @@
+import calc
+def main():
+    print(calc.sumar(4,2))
+    print(calc.restar(4,2))
+    print(calc.multiplicar(4,2))
+    print(calc.dividir(4,2))
+if __name__ == '__main__':
+    main()
