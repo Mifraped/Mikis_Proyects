ba41f21c123e77a3289d1127425d909a4d9d679d
miki <francamente@hotmail.es>
2022-12-16 19:28:32 +0100

    Ejercicio01JS

---
+++ Ejercicio01JS.js
@@ -0,0 +1,13 @@
+let nombre = "Miguel";
+let edad = 38;
+let desarrollador = false;
+let fechaNacimiento = new Date(1984, 08, 07);
+let libro = {
+    titulo: "1984",
+    autor: "George Orwell",
+    año: 1948,
+    url: "https://www.amazon.es/1984-Contempor%C3%A1nea-CONTEMPORANEA-George-Orwell/dp/8499890946/ref=asc_df_8499890946/?tag=googshopes-21&linkCode=df0&hvadid=195225763566&hvpos=&hvnetw=g&hvrand=13391634188131427456&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1005493&hvtargid=pla-138218692395&psc=1"
+}
+
+const lista = [nombre , edad , desarrollador , fechaNacimiento, libro];
+console.log(lista);
