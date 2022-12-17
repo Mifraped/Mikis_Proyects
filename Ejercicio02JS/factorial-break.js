c97cd2fb6301b58ecf74d1e62a73e914b8657e51
miki <francamente@hotmail.es>
2022-12-17 20:30:39 +0100

    Ejercicio02JS

---
+++ Ejercicio02JS/factorial-break.js
@@ -0,0 +1,10 @@
+let x = 1
+let i = 1
+while (true){
+    x = x * i
+    if(i === 10){
+        break
+    }
+    i++
+}
+console.log(x)
---
+++ Ejercicio02JS/factorial-while.js
@@ -0,0 +1,7 @@
+let x = 1
+let i = 1
+while (i <= 10){
+    x = x * i
+    i++
+}
+console.log(x)
