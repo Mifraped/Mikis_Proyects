0432ef5f20669576f0c32fc8e2dd6382f674d14c
miki <francamente@hotmail.es>
2022-12-18 21:47:23 +0100

    Ejercicio04JS

---
+++ Ejercicio04JS.js
@@ -0,0 +1,7 @@
+let alturaCm = 175
+let alturaM = 1.75
+let pesoKg = 70.6
+let altMred = Math.ceil(alturaM)
+let pesokgred = Math.floor(pesoKg)
+let siOno = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
